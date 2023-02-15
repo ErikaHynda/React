72074da0e5d9e3eb114f15a3d05e2150d4e0f6e0
ErikaHynda <erfink22@gmail.com>
2023-02-15 17:36:08 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import {Simpsons} from "./components/Simpsons/Simpsons";
-// import {Simpson} from "./components/Simpson/Simpson";
-
 const App = () => {
 
     return (
 <div>
-    <Simpsons/>
+
 </div>
     );
 };
